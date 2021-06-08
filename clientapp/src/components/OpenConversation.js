c779be12c5c410981821d47b9be18bee7c398c76
avneet5658 <avneet.pandey@mail.vinove.com>
2021-06-08 08:46:20 +0530

    Application completed

--- clientapp/src/components/OpenConversation.js
+++ clientapp/src/components/OpenConversation.js
@@ -1,21 +1,60 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { Form, FormControl, InputGroup, Button } from "react-bootstrap";
 import { useConversation } from "../contexts/ConversationsProvider";
 const OpenConversation = () => {
   const [text, setText] = useState("");
+  const lastMessageRef = useRef();
   const { sendMessage, selectedConversation } = useConversation();
 
   function handleSubmit(e) {
-    e.prevetDefault();
+    e.preventDefault();
     sendMessage(
       selectedConversation.recipients.map((r) => r.id),
       text
     );
     setText("");
   }
+  useEffect(() => {
+    if (lastMessageRef.current) {
+      lastMessageRef.current.scrollIntoView({ smooth: true });
+    }
+  });
   return (
     <div className="d-flex flex-column flex-grow-1">
-      <div className="flex-grow-1 overflow-auto"></div>
+      <div className="flex-grow-1 overflow-auto">
+        <div className="h-100 d-flex flex-column align-items-start justify-content-end px-3"></div>
+        {console.log(selectedConversation)}
+        {selectedConversation.messages.map((message, index) => {
+          const lastMessage =
+            selectedConversation.messages.length - 1 === index;
+          return (
+            <div
+              key={index}
+              ref={lastMessage ? lastMessageRef : null}
+              className={`my-1 d-flex ${
+                message.fromMe ? "justify-content-end" : ""
+              }`}
+            >
+              <div>
+                <div
+                  className={`rounded px-2 py-1 ${
+                    message.fromMe ? "bg-primary text-white" : "border"
+                  }`}
+                >
+                  {message.text}
+                </div>
+                <div
+                  className={`text-muted small ${
+                    message.fromMe ? "text-right" : ""
+                  }`}
+                >
+                  {message.fromMe ? "You" : message.senderName}
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
       <Form onSubmit={handleSubmit}>
         <Form.Group className="m-2">
           <InputGroup>
